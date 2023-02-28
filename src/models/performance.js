d98cf07d4bac9c1efd066d34d1192e4338049750
shigni <louisservant86@gmail.com>
2023-02-28 23:00:35 +0100

    Upper case Performances

--- src/models/performance.js
+++ src/models/performance.js
@@ -1,11 +1,18 @@
 export class PerformanceParser {
+  static upperCaseFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   static parse(apiData) {
     const result = [];
     const data = apiData.data;
     const kind = apiData.kind;
 
     data.forEach((d) => {
-      const performance = { value: d.value, kind: kind[d.kind] };
+      const performance = {
+        value: d.value,
+        kind: PerformanceParser.upperCaseFirst(kind[d.kind]),
+      };
       result.push(performance);
     });
     console.log(result);
